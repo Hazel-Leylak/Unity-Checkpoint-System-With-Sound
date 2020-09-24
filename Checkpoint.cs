8934e552fd37f7046766683542d81e494004fe24
Hazel <47564151+Hazel-Leylak@users.noreply.github.com>
2020-09-24 16:54:43 +0300

    Add files via upload

---
+++ Checkpoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public GameObject[] Checkpoints;
+    public GameObject FirstParkingBound;
+    int count;
+    AudioSource checkp;
+
+    private void Start()
+    {
+        checkp = GetComponent<AudioSource>();
+        checkp.Stop();
+    }
+
+
+    public void OpenNext(Collider c)
+    {
+        
+        Checkpoints[count].SetActive(false);
+        checkp.Play();
+        if (count < Checkpoints.Length-1)
+        {
+            count++;
+            Checkpoints[count].SetActive(true);            
+        }
+        
+        else if (count == Checkpoints.Length-1)
+        {
+            FirstParkingBound.SetActive(true);
+        } 
+    }
+}
---
+++ ChildCheckpoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildCheckpoint : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+           gameObject.GetComponentInParent<Checkpoint>().OpenNext(c);            
+        }
+    }
+}
